bd26a17f314d9cf691e1d2dd58ffda009581cabe
Mansi-b5 <60612351+Mansi-b5@users.noreply.github.com>
2025-05-31 00:02:21 +1000

    Added zookeeper class

---
+++ zookeeper.py
@@ -0,0 +1,16 @@
+# Used week 7 tutorial as inspiration 
+# Wk7_kazoo_ZK.py
+
+from kazoo.client import KazooClient
+from kazoo.exceptions import KazooException
+import threading
+import time, json
+
+class ZookeeperManager:
+    def __init__(self): 
+        self.id=threading.get_ident()
+        self.zk = KazooClient(hosts='127.0.0.1:2181') 
+        self.zk.start()
+
+
+
